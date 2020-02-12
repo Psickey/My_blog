3b611e3e1a61c3d44cc28b401f754e9ff0d95fb3
Psickey <pkeynevernaton@gmail.com>
2020-02-12 22:47:51 +0700

    Statics

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
