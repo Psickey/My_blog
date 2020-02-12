88d0931787711e3a643a566ad9a199cfec4f6611
Psickey <pkeynevernaton@gmail.com>
2020-02-12 15:45:19 +0700

    Deleted unused css style

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,13 +147,12 @@ STATIC_URL = '/static/'
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# SMTP server settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# SMTP server settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
